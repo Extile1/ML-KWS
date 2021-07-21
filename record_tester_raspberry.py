305c9141fc07cb0c7aeab4d0f2de3e497a74d179
Extile1 <59020388+Extile1@users.noreply.github.com>
2021-07-20 20:16:28 -0400

    Debug

--- record_tester_raspberry.py
+++ record_tester_raspberry.py
@@ -4,6 +4,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import sounddevice as sd
 from scipy.io.wavfile import write
+import time
+import struct
 
 chunk = 8000  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
@@ -51,9 +53,56 @@ filename = "output_raspberry_3"
 
 print("Recording")
 
-myrecording = sd.rec(int(seconds * record_fs), samplerate=record_fs, channels=channels)
-sd.wait()  # Wait until recording is finished
-write(filename + '.wav', record_fs, myrecording)  # Save as WAV file
+# myrecording = sd.rec(int(seconds * record_fs), samplerate=record_fs, channels=channels)
+# sd.wait()  # Wait until recording is finished
+# write(filename + '.wav', record_fs, myrecording)  # Save as WAV file
 
-plt.scatter(list(map(lambda x: x * record_fs / record_fs, range(len(myrecording)))), myrecording, s = [0.5] * len(myrecording))
+# plt.scatter(list(map(lambda x: x * record_fs / record_fs, range(len(myrecording)))), myrecording, s = [0.5] * len(myrecording))
+# plt.show()
+
+p = pyaudio.PyAudio()
+
+frames = []
+index = 0
+next_index = 0 #the next index outside the window
+
+def callback(in_data, frame_count, time_info, flag):
+    global frames, total, index, not_extreme, count, next_index, length_1, length_2 #global variables for filter coefficients and array\
+    length_1 += frame_count
+    data = np.fromstring(in_data, np.int16)
+    data = data.tolist()
+    frames.extend(data)
+
+    if length_1 >= record_fs * seconds * channels:
+        return in_data, pyaudio.paComplete
+    else:
+        return in_data, pyaudio.paContinue
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=record_fs,
+                #frames_per_buffer=chunk,
+                #input_device_index=1,
+                input=True,
+                stream_callback=callback)
+
+stream.start_stream()
+
+while stream.is_active():
+    time.sleep(0.1)
+stream.close()
+
+p.terminate()
+
+print('Finished recording')
+
+plt.scatter(list(map(lambda x: x * record_fs / record_fs, range(len(frames)))), frames, s = [1] * len(frames))
 plt.show()
+# Save the recorded data as a WAV file
+wf = wave.open(filename + ".wav", 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(record_fs)
+format = "%dh" % (len(frames))
+wf.writeframes(struct.pack(format, *frames))
+wf.close()
